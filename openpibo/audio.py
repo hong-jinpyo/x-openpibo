5af30902749e4a52249595f58672a452820611af
Jinpyo-Hong <hojp7874@circul.us>
2021-08-17 16:31:12 +0900

    create audio sample docs

--- openpibo/audio.py
+++ openpibo/audio.py
@@ -1,6 +1,6 @@
 """
 Audio
-~~~~~
+-----
 """
 
 
@@ -16,9 +16,10 @@ class Audio:
 
   파라미터를 받지 않습니다.
 
-  example:
-  .. code-block::
-      pibo_audio = Audio()
+  example::
+
+    pibo_audio = Audio()
+
   """
   # out: local/hdmi/both
   # volume: mdB
@@ -31,26 +32,35 @@ class Audio:
     """
     입력한 경로의 파일을 재생합니다.
 
-    :param str filename: 재생할 파일의 경로.
-        `mp3`와 `wav` 형식을 지원한다.
+    example::
 
-        example:
-        .. code-block::
-            '/home/pi/data/audio/opening.mp3'
+      pibo_audio.play('/home/pi/data/audio/opening.mp3')
+        
+    :param str filename: 재생할 파일의 경로.
+    
+      ``mp3`` 와 ``wav`` 형식을 지원합니다.
 
     :param str out: 어느 포트에서 재생할지 선택합니다.
-        'local', 'hdmi', 'both'만 입력할 수 있습니다.
-        default는 'local'.
+    
+      ``local``, ``hdmi``, ``both`` 만 입력할 수 있습니다.
+      
+      (default: ``local``)
 
-    :param str volumn: 볼륨을 설정한다.
-        default는 '-2000'이며, 값이 커질수록 볼륨이 커집니다.
-        해당 값을 양수로 키우면 볼륨이 매우 커지므로 음수로 사용하는 것을 권장합니다.
+    :param str or int volume: 볼륨을 설정합니다.
+    
+      단위는 mdB 이고, 값이 커질수록 볼륨이 커집니다.
+      
+      해당 값을 양수로 키우면 볼륨이 매우 커지므로 음수로 사용하는 것을 권장합니다.
+
+      (default: ``-2000``)
 
     :param bool background: 오디오 파일을 백그라운드에서 실행할지 여부를 결정합니다.
-        값이 'False'이면, 오디오 파일이 종료될 때 까지 다른 명령어를 실행할 수 없고,
-        값이 'True'이면, 오디오 재생 중에 다른 명령어를 사용할 수 있습니다.
-        default는 'True'.
+
+      * ``True``: 오디오 재생 중에 다른 명령어를 사용할 수 있습니다. (default)
+      * ``False``: 오디오 파일이 종료될 때 까지 다른 명령어를 실행할 수 없습니다.
+
     """
+
     if background:
       os.system("omxplayer -o {} --vol {} {} &".format(out, volume, filename))
     else:
@@ -58,14 +68,19 @@ class Audio:
 
   def stop(self):
     """background에서 재생중인 오디오를 정지합니다.
+    
     파라미터를 받지 않습니다."""
+
     os.system('sudo pkill omxplayer')
   
   def mute(self, value):
     """파이보를 무음모드로 만듭니다.
     
     :param bool value:
-        'True'이면 무음모드, 'False'이면 무음모드 해제."""
+    
+      * ``True``: 무음모드
+      * ``False``: 무음모드 해제."""
+
     if type(value) != bool:
       raise TypeError(f"'{value}' is not a bool.")
     if value:
